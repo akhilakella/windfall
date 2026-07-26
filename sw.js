0e9e517c9f189ca7cc15a61ecf2f2d3432e8a5e9
akhilakella <akhilakella@outlook.com>
2026-07-26 22:15:39 +0100

    Expand onboarding tour to eight slides: fruit checker, responsible picking, notifications, and the community goal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "windfall-v13";
+const CACHE = "windfall-v14";
 const STATIC = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json", "/icon.svg", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
